b77718602b7cb46ae8aca1239a122b10add4a4f1
Matheus Arakaki <arakakimath@gmail.com>
2024-10-16 19:03:10 -0400

    deleting and verifying if the id is valid

--- src/controllers/products-controller.ts
+++ src/controllers/products-controller.ts
@@ -1,10 +1,12 @@
 import { NextFunction, Request, Response } from "express";
 import z from "zod";
 import { knex } from "@/database/knex";
+import { AppError } from "@/utils/AppError";
 
 class ProductController {
-  async index(request: Request, response: Response, next: NextFunction){
+  index = async (request: Request, response: Response, next: NextFunction) => {
     try {
+      console.log(this)
       const { name } = request.query
 
       const products = await knex<ProductRepository>("products")
@@ -18,7 +20,7 @@ class ProductController {
     }
   }
 
-  async create(request: Request, response: Response, next: NextFunction){
+  create = async (request: Request, response: Response, next: NextFunction) => {
     try {
       const bodySchema = z.object({
         name: z.string().trim().min(6),
@@ -35,9 +37,15 @@ class ProductController {
     }
   }
 
-  async update(request: Request, response: Response, next: NextFunction){
+  update = async (request: Request, response: Response, next: NextFunction) => {
     try {
-      const id = z.string().transform((value) => Number(value)).refine((value) => !isNaN(value), {message: "id must be a number"}).parse(request.params.id)
+      const id = z
+        .string()
+        .transform((value) => Number(value))
+        .refine((value) => !isNaN(value), {message: "id must be a number"})
+        .parse(request.params.id)
+
+      await this.#verifyId(id); // Verify if id exists
 
       const bodySchema = z.object({
         name: z.string().trim().min(6),
@@ -54,6 +62,37 @@ class ProductController {
     } catch (error) {
       next(error)
     }
+  }
+
+  delete = async (request: Request, response: Response, next: NextFunction) => {
+    try {
+      const id = z
+        .string()
+        .transform((value) => Number(value))
+        .refine((value) => !isNaN(value), {message: "id must be a number"})
+        .parse(request.params.id)
+
+      await this.#verifyId(id); // Verify if id exists
+
+      await knex<ProductRepository>("products")
+        .delete()
+        .where({ id })
+
+      return response.json()
+    } catch (error) {
+      next(error)
+    }
+  }
+
+  #verifyId = async (id) => {
+    console.log(this)
+    const product = await knex<ProductRepository>("products")
+      .select()
+      .where({ id })
+      .first() // To product become an object instead of an array of objects
+
+    if(!product) // Verify if there is a product with the id 
+      throw new AppError("product not found")
   }
 }
 
--- src/routes/products-routes.ts
+++ src/routes/products-routes.ts
@@ -2,10 +2,11 @@ import { ProductController } from "@/controllers/products-controller";
 import { Router } from "express";
 
 const productsRoutes = Router()
-const productsController = new ProductController
+const productsController = new ProductController()
 
 productsRoutes.get("/", productsController.index)
 productsRoutes.post("/", productsController.create)
 productsRoutes.put("/:id", productsController.update)
+productsRoutes.delete("/:id", productsController.delete)
 
 export { productsRoutes }
